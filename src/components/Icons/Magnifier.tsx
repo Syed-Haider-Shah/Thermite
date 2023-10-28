8ef87f028fa12d61ad375515da982bc3c93cabb1
Ali-Aizaz <50923380+Ali-Aizaz@users.noreply.github.com>
2023-10-28 17:05:44 +0500

    created parent tickets page

--- src/components/Icons/Magnifier.tsx
+++ src/components/Icons/Magnifier.tsx
@@ -4,8 +4,8 @@ const Magnifier = ({ className }: ISvgIcon) => (
   <svg
     className={className}
     xmlns="http://www.w3.org/2000/svg"
-    width="12"
-    height="12"
+    width="24"
+    height="24"
     viewBox="0 0 12 12"
     fill="none"
   >

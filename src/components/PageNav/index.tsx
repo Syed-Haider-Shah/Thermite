47400edaa4d763233697bf290a4a221844a0f119
Ali-Aizaz <50923380+Ali-Aizaz@users.noreply.github.com>
2023-11-18 15:00:06 +0500

    fixed PageNav

--- src/components/PageNav/index.tsx
+++ src/components/PageNav/index.tsx
@@ -1,46 +1,69 @@
 'use client'
 
 import Link from 'next/link'
-import { useParams, useRouter } from 'next/navigation'
+import { usePathname, useSearchParams } from 'next/navigation'
 
 import { memo, useCallback } from 'react'
 
-import { Button } from '@/components'
 import { cn } from '@/utils/cn'
 
 const PageNav = ({ pageCount }: { pageCount: number }) => {
-  const { page } = useParams() || { page: null }
-  const navigate = useRouter()
+  const searchParams = useSearchParams()
+  const page = searchParams.get('page') || '1'
+  const pathname = usePathname()
+
+  const handlePageSelect = useCallback(
+    (val: number) => {
+      const newParams = new URLSearchParams(searchParams)
+      newParams.set('page', `${val}`)
+      return newParams.toString()
+    },
+    [searchParams]
+  )
 
   const handleNext = useCallback(() => {
-    if (typeof page === 'string') {
-      const currPage = parseInt(page, 10)
-      if (page && currPage < pageCount) navigate.push(`./../${currPage + 1}`)
-    }
-  }, [navigate, page, pageCount])
+    const newParams = new URLSearchParams(searchParams)
+    const pageNum = Number(page)
+
+    if (pageNum >= pageCount) newParams.set('page', `${pageNum}`)
+    else if (pageNum && pageNum > 1) newParams.set('page', `${pageNum + 1}`)
+    else newParams.set('page', '2')
+
+    return newParams.toString()
+  }, [page, pageCount, searchParams])
 
   const handlePrev = useCallback(() => {
-    if (typeof page === 'string') {
-      const currPage = parseInt(page, 10)
-      if (page && currPage > 1) navigate.push(`./../${currPage - 1}`)
-    }
-  }, [navigate, page])
+    const newParams = new URLSearchParams(searchParams)
+    const pageNum = Number(page)
+
+    if (pageNum && pageNum > 1 && pageNum <= pageCount)
+      newParams.set('page', `${pageNum - 1}`)
+    else newParams.set('page', '1')
+
+    return newParams.toString()
+  }, [page, pageCount, searchParams])
 
   return (
     <div className="flex items-center justify-center gap-x-2 shadow-md">
-      <Button
-        onClick={handlePrev}
-        className="h-7 rounded-md border border-darkGray bg-white px-2 text-black/50"
+      <Link
+        href={{
+          pathname,
+          query: handlePrev()
+        }}
+        className="h-7 rounded-md border border-darkGray bg-white px-2 pt-0.5 text-black/50"
       >
         Prev
-      </Button>
+      </Link>
       {[...Array(pageCount)].map((e, val) => {
         const pageVal = val + 1
         return (
           <Link
             key={pageVal}
-            href={`./../${pageVal}`}
-            className={cn('rounded px-1 font-semibold', {
+            href={{
+              pathname,
+              query: handlePageSelect(pageVal)
+            }}
+            className={cn('rounded px-1 pt-1 font-semibold', {
               'border border-darkGray text-black': `${pageVal}` === page,
               'text-black/50': `${pageVal}` !== page
             })}
@@ -49,12 +72,15 @@ const PageNav = ({ pageCount }: { pageCount: number }) => {
           </Link>
         )
       })}
-      <Button
-        onClick={handleNext}
-        className="h-7 rounded-md border border-darkGray bg-white px-2 text-black/50"
+      <Link
+        href={{
+          pathname,
+          query: handleNext()
+        }}
+        className="h-7 rounded-md border border-darkGray bg-white px-2 pt-0.5 text-black/50"
       >
         Next
-      </Button>
+      </Link>
     </div>
   )
 }

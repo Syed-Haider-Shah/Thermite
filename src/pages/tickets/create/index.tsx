d3a1ef828d43cbe62d44510fa6ec6f176c1bf331
Ali-Aizaz <50923380+Ali-Aizaz@users.noreply.github.com>
2023-11-19 14:13:47 +0500

    added customers table data

--- src/pages/tickets/create/index.tsx
+++ src/pages/tickets/create/index.tsx
@@ -1,7 +1,11 @@
 import { useRouter } from 'next/navigation'
 
+import { useCallback, useEffect, useState } from 'react'
+
 import { Button, SearchBar, Table } from '@/components'
 import { Modal } from '@/containers'
+import { supabase } from '@/services/supabase'
+import { ICustomer } from '@/types/supabaseTables'
 
 const cols = [
   {
@@ -20,12 +24,35 @@ const cols = [
 
 const CreateParentTicket = () => {
   const router = useRouter()
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [customers, setCustomers] = useState<ICustomer[]>([])
+
+  const fetchCustomers = useCallback(async () => {
+    setIsLoading(true)
+    const { data: rows, error } = await supabase
+      .from('Customers')
+      .select()
+      .limit(15)
+    setIsLoading(false)
+    if (error) {
+      console.log(error.message)
+      return
+    }
+
+    if (rows) {
+      setCustomers(rows as ICustomer[])
+    }
+  }, [])
+
+  useEffect(() => {
+    fetchCustomers()
+  }, [fetchCustomers])
 
   return (
     <Modal showModal title="Create Parent Ticket" onClose={router.back}>
-      <form className="relative space-y-5 rounded-xl pb-14 pt-5">
+      <form className="relative space-y-5 overflow-hidden rounded-xl pb-14 pt-5">
         <SearchBar placeholder="Search for Customers" />
-        <Table cols={cols} rows={[]} />
+        <Table cols={cols} rows={customers} isLoading={isLoading} />
         <Button active className="absolute right-0">
           Create
         </Button>

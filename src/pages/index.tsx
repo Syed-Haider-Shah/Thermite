466a336557c9088b3a750e30be9a7697aa1ec960
Ali-Aizaz <50923380+Ali-Aizaz@users.noreply.github.com>
2023-11-23 13:31:14 +0500

    added login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,47 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
+
+import { useCallback } from 'react'
+import { useForm } from 'react-hook-form'
+
+import { yupResolver } from '@hookform/resolvers/yup'
 
 import { Button, FormLine } from '@/components'
+import { Paths } from '@/constants'
+import { supabase } from '@/services/supabase'
+import { LoginSchema } from '@/utils/yupConfig'
 
 export default function Home() {
+  const router = useRouter()
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit
+  } = useForm({
+    resolver: yupResolver(LoginSchema),
+    mode: 'onBlur',
+    defaultValues: {
+      email: '',
+      password: ''
+    }
+  })
+
+  const handleSignIn = useCallback(
+    async ({ email, password }: { email: string; password: string }) => {
+      const { error } = await supabase.auth.signInWithPassword({
+        email,
+        password
+      })
+
+      if (error) console.log(error)
+
+      router.push(Paths.HOME)
+    },
+    [router]
+  )
+
   return (
     <>
       <Head>
@@ -12,11 +50,16 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <div className="relative flex w-full justify-center ">
-        <form className="flex w-full max-w-md flex-col items-center gap-7 rounded-5 bg-white py-[12%]">
+        <form
+          onSubmit={handleSubmit(handleSignIn)}
+          className="flex w-full max-w-md flex-col items-center gap-7 rounded-5 bg-white py-[12%]"
+        >
           <Image src="/logo.svg" alt="Logo" width={150} height={150} />
           <FormLine
             id="email"
             title="Email"
+            {...register('email')}
+            error={errors.email?.message}
             required
             className="w-80"
             placeholder="Email"
@@ -24,6 +67,9 @@ export default function Home() {
           <FormLine
             id="password"
             title="Password"
+            {...register('password')}
+            error={errors.password?.message}
+            type="password"
             required
             className="w-80"
             placeholder="Password"

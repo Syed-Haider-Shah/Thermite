037065cf98c39aa85b484a9fb6ed607bb13b9e3c
Ali-Aizaz <50923380+Ali-Aizaz@users.noreply.github.com>
2023-12-30 06:26:35 +0500

    added upload file function

---
+++ src/utils/uploadFile.ts
@@ -0,0 +1,34 @@
+import toast from 'react-hot-toast'
+
+import { supabaseStorageUrl } from '@/constants'
+import { supabase } from '@/services/supabase'
+
+export const uploadImage = async (
+  photoFile: File,
+  folder: 'avatars' | 'post_images',
+  name: string
+) => {
+  const bucket = supabase.storage.from('thermite_bucket')
+  const fileName = `${folder}/${name}`
+
+  const { data, error } = await bucket.upload(fileName, photoFile, {
+    cacheControl: '3600',
+    upsert: true
+  })
+
+  if (error) toast.error(error.message)
+  else if (data)
+    return {
+      url: `${supabaseStorageUrl}/${fileName}`,
+      onDelete: async () => {
+        const { error: delError } = await bucket.remove([fileName])
+
+        if (delError) toast.error(delError.message)
+      }
+    }
+
+  return {
+    url: '',
+    onDelete: async () => {}
+  }
+}

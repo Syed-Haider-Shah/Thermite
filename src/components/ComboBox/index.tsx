ba1ec8404521aaa915c9327c4e1d30cae1a90f39
Ali-Aizaz <50923380+Ali-Aizaz@users.noreply.github.com>
2024-01-16 04:01:41 +0500

    added ticket search on water sample form

--- src/components/ComboBox/index.tsx
+++ src/components/ComboBox/index.tsx
@@ -1,42 +1,103 @@
+import { ChangeEvent, useCallback, useRef, useState } from 'react'
+import Skeleton from 'react-loading-skeleton'
+
+import debounce from 'lodash.debounce'
+
 import { IRow } from '@/types/supabaseTables'
 import { cn } from '@/utils/cn'
 
 import FormLine from '../FormLine'
 
+type IComboBox = {
+  items: IRow[]
+  title: string
+  placeholder?: string
+  field: string
+  isLoading?: boolean
+  onSelect?: (val: string) => void
+  onSearch?: (val: string) => void
+}
+
 const ComboBox = ({
   items,
   title,
-  place
-}: {
-  items: IRow[]
-  title: string
-  place?: string
-}) => (
-  <div
-    id="dropdown-menu"
-    className="right-0 mt-2 w-full space-y-1 rounded-md bg-white p-1"
-  >
-    <fieldset
-      className={cn(
-        'box-border rounded-lg border-4',
-        'border-loadGray focus-within:border-loadBlue'
-      )}
-    >
-      <div className="w-fit -translate-y-3 translate-x-6 bg-white px-1 text-sm">
-        {title}
-      </div>
-      <FormLine id="ticket" placeholder={place} cusForm />
-    </fieldset>
+  placeholder,
+  field,
+  isLoading,
+  onSelect,
+  onSearch
+}: IComboBox) => {
+  const [search, setSearch] = useState<string>('')
 
-    {items.map((item) => (
-      <div
-        key={`${item}`}
-        className="text-gray-700 hover:bg-gray-100 active:bg-blue-100 block cursor-pointer rounded-md px-4 py-2"
+  const debouncedFn = useRef(
+    debounce(onSearch ? onSearch : () => {}, 500)
+  ).current
+
+  const handleChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      setSearch(event.target.value)
+      debouncedFn(event.target.value)
+    },
+    [debouncedFn]
+  )
+
+  const handleSelect = useCallback(
+    (val: IRow) => {
+      setSearch(val[field] as string)
+      if (onSelect) onSelect(val['id'] as string)
+    },
+    [field, onSelect]
+  )
+
+  return (
+    <div
+      id="dropdown-menu"
+      className="relative right-0 mt-2 w-full space-y-1 rounded-md bg-white p-1"
+    >
+      <fieldset
+        className={cn(
+          'box-border rounded-lg border-4',
+          'border-loadGray focus-within:border-loadBlue'
+        )}
       >
-        Uppercase
-      </div>
-    ))}
-  </div>
-)
+        <div className="w-fit -translate-y-3 translate-x-6 bg-white px-1 text-sm">
+          {title}
+        </div>
+        <FormLine
+          id="ticket"
+          placeholder={placeholder}
+          value={search}
+          cusForm
+          onChange={handleChange}
+        />
+      </fieldset>
+      <ul
+        className={cn(
+          'absolute z-10 w-[calc(100%-10px)] overflow-hidden rounded-md border border-black/10 bg-white',
+          { 'border-0': items.length < 1 && !isLoading }
+        )}
+      >
+        {items.map((item) => (
+          // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
+          <li
+            onClick={() => {
+              handleSelect(item)
+            }}
+            key={`${item}`}
+            className="block cursor-pointer border-b border-black/5 px-4 py-2 last:border-b-0 hover:bg-black/10"
+          >
+            {item[field]}
+          </li>
+        ))}
+        {isLoading && (
+          <li className="block cursor-pointer border-b border-black/5 px-4 py-2 last:border-b-0 hover:bg-black/10">
+            <Skeleton className="" />
+            <Skeleton className="" />
+          </li>
+        )}
+      </ul>
+    </div>
+  )
+}
 
 export default ComboBox
--- src/pages/forms/water-sample-form/index.tsx
+++ src/pages/forms/water-sample-form/index.tsx
@@ -1,16 +1,65 @@
-import { ComboBox, DatePicker, RadioButton, TextArea } from '@/components'
+import { useCallback, useState } from 'react'
+import toast from 'react-hot-toast'
+
+import {
+  Button,
+  ComboBox,
+  DatePicker,
+  RadioButton,
+  TextArea
+} from '@/components'
+import { supabase } from '@/services/supabase'
+import { IRow } from '@/types/supabaseTables'
 import { cn } from '@/utils/cn'
 
 const WaterForm = () => {
+  const [ticketList, setTicketList] = useState<IRow[]>([])
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [selected, setSelected] = useState<string>('')
+
+  const fetchTickets = useCallback(async (search: string) => {
+    setIsLoading(true)
+
+    const { data, error } = await supabase
+      .rpc('get_parent_tickets')
+      .eq('status', 'WATER-SAMPLE')
+      .ilike('address', `%${search}%`)
+      .select('address, status, id')
+
+    setIsLoading(false)
+
+    if (data) setTicketList(data)
+    else if (error) toast.error(error.message)
+  }, [])
+
+  const handleSelect = useCallback((val: string) => {
+    setSelected(val)
+    setTicketList([])
+  }, [])
+
+  const handleSubmit = useCallback(() => {
+    selected // contains the ID of the parent ticket
+  }, [selected])
+
   return (
     <div className=" mx-20 h-screen w-form bg-white shadow-lg">
       <div className="m-10 ml-40 mt-24 text-xl font-bold text-darkIndigo">
         Water Sample Form
       </div>
-      <div className="mx-auto grid h-20 w-4/5 grid-cols-2 place-items-center gap-7">
-        <ComboBox title="Ticket Name" items={[]} />
-        <ComboBox title="Region" items={[]} />
-        <ComboBox title="Panel Generation" items={[]} />
+      <form
+        onSubmit={handleSubmit}
+        className="mx-auto grid h-20 w-4/5 grid-cols-2 place-items-center gap-7"
+      >
+        <ComboBox
+          title="Ticket Name"
+          items={ticketList}
+          field="address"
+          isLoading={isLoading}
+          onSearch={fetchTickets}
+          onSelect={handleSelect}
+        />
+        <ComboBox title="Region" items={[]} field="region" />
+        <ComboBox title="Panel Generation" items={[]} field="panel" />
         <div className="flex w-full flex-col justify-center pl-10">
           <div className="flex text-sm">
             Where did you collect the sample from?
@@ -24,7 +73,7 @@ const WaterForm = () => {
         <DatePicker title="Water Test Vial Expiration Date" />
         <DatePicker title="Water Test Sample Date & Time" showTime />
         <DatePicker title="Water Test Read Date & Time" showTime />
-        <ComboBox title="Water Test Sample Result" items={[]} />
+        <ComboBox title="Water Test Sample Result" items={[]} field="" />
         <div className="flex w-full translate-x-8 flex-col gap-2 ">
           <div className="text-md w-fit bg-white px-1 font-bold">
             Standard Water Test Results Image (No UV Black Light)
@@ -47,10 +96,13 @@ const WaterForm = () => {
           </div>
           <TextArea id="ticket" placeholder="Select a Ticket" custForm />
         </fieldset>
-        <button className="text-md col-span-2 w-1/2 rounded bg-loadBlue p-2 px-56 font-bold text-white">
+        <Button
+          type="submit"
+          className="col-span-2 w-1/2 rounded bg-loadBlue p-2 font-bold text-white"
+        >
           SUBMIT
-        </button>
-      </div>
+        </Button>
+      </form>
     </div>
   )
 }

cd6e1e54b6cdc1155044b04e0fd3b5a6d4467ddc
Syed-Haider-Shah <140457553+Syed-Haider-Shah@users.noreply.github.com>
2024-01-19 21:48:41 +1100

    Water Sampling form full responsive for mobile view

--- src/pages/forms/water-sample-form/index.tsx
+++ src/pages/forms/water-sample-form/index.tsx
@@ -42,68 +42,78 @@ const WaterForm = () => {
   }, [selected])
 
   return (
-    <div className=" mx-20 h-screen w-form bg-white shadow-lg">
-      <div className="m-10 ml-40 mt-24 text-xl font-bold text-darkIndigo">
+    <div className=" max-w-form flex h-auto flex-col bg-white pb-20 shadow-lg md:mx-20">
+      <div className="m-10 mt-24 flex flex-wrap text-xl font-bold text-darkIndigo md:ml-40">
         Water Sample Form
       </div>
       <form
         onSubmit={handleSubmit}
-        className="mx-auto grid h-20 w-4/5 grid-cols-2 place-items-center gap-10"
+        className="mx-auto flex h-full w-4/5 flex-col items-center gap-8 bg-white "
       >
-        <ComboBox
-          title="Ticket Name"
-          items={ticketList}
-          field="address"
-          isLoading={isLoading}
-          onSearch={fetchTickets}
-          onSelect={handleSelect}
-        />
-        <ComboBox title="Region" items={[]} field="region" />
-        <ComboBox title="Panel Generation" items={[]} field="panel" />
-        <div className="flex w-full flex-col justify-center gap-2 pl-1">
-          <div className="flex text-sm">
-            Where did you collect the sample from?
+        <div className="flex w-full flex-wrap justify-between space-y-8 bg-white md:space-y-0">
+          <div className="flex min-w-[16rem] flex-col gap-8 md:w-[32rem]">
+            <ComboBox
+              title="Ticket Name"
+              items={ticketList}
+              field="address"
+              isLoading={isLoading}
+              onSearch={fetchTickets}
+              onSelect={handleSelect}
+            />
+            <ComboBox title="Panel Generation" items={[]} field="panel" />
+            <DatePicker
+              id="date-picker1"
+              title="Water Test Vial Expiration Date"
+            />
+            <DatePicker
+              id="date-picker3"
+              title="Water Test Read Date & Time"
+              showTime
+            />
+            <div className="flex w-full flex-col gap-2 ">
+              <div className="text-md w-fit bg-white px-1 font-bold">
+                Standard Water Test Results Image (No UV Black Light)
+              </div>
+              <div className="flex items-center gap-5">
+                <button className="rounded bg-loadBlue p-2 text-sm text-white">
+                  Upload Water Sample
+                </button>
+                <div className="text-gray">No File Chosen</div>
+              </div>
+            </div>
           </div>
-          <div className="flex gap-5 pl-1 text-xs">
-            <RadioButton name="HUB" />
-            <RadioButton name="SPOKE" />
-            <RadioButton name="BOTH" />
+          <div className="flex min-w-[20rem] flex-col gap-8 md:w-[32rem]">
+            <ComboBox title="Region" items={[]} field="region" />
+            <div className="flex w-full flex-col justify-center gap-2 pl-1">
+              <div className="flex text-sm">
+                Where did you collect the sample from?
+              </div>
+              <div className="flex gap-5 pl-1 text-xs">
+                <RadioButton name="HUB" />
+                <RadioButton name="SPOKE" />
+                <RadioButton name="BOTH" />
+              </div>
+            </div>
+            <DatePicker
+              id="date-picker2"
+              title="Water Test Sample Date & Time"
+              showTime
+            />
+            <ComboBox title="Water Test Sample Result" items={[]} field="" />
+            <fieldset
+              className={cn(
+                'box-border w-full rounded-lg border-4',
+                'border-loadGray focus-within:border-loadBlue'
+              )}
+            >
+              <div className="w-fit -translate-y-3 translate-x-6 bg-white px-1 text-sm">
+                Comments
+              </div>
+              <TextArea id="ticket" placeholder="Select a Ticket" custForm />
+            </fieldset>
           </div>
         </div>
-        <DatePicker id="date-picker1" title="Water Test Vial Expiration Date" />
-        <DatePicker
-          id="date-picker2"
-          title="Water Test Sample Date & Time"
-          showTime
-        />
-        <DatePicker
-          id="date-picker3"
-          title="Water Test Read Date & Time"
-          showTime
-        />
-        <ComboBox title="Water Test Sample Result" items={[]} field="" />
-        <div className="flex w-full flex-col gap-2 ">
-          <div className="text-md w-fit bg-white px-1 font-bold">
-            Standard Water Test Results Image (No UV Black Light)
-          </div>
-          <div className="flex items-center gap-5">
-            <button className="rounded bg-loadBlue p-2 text-sm text-white">
-              Upload Water Sample
-            </button>
-            <div className="text-gray">No File Chosen</div>
-          </div>
-        </div>
-        <fieldset
-          className={cn(
-            'box-border w-full rounded-lg border-4',
-            'border-loadGray focus-within:border-loadBlue'
-          )}
-        >
-          <div className="w-fit -translate-y-3 translate-x-6 bg-white px-1 text-sm">
-            Comments
-          </div>
-          <TextArea id="ticket" placeholder="Select a Ticket" custForm />
-        </fieldset>
+
         <Button
           type="submit"
           className="col-span-2 w-1/2 rounded bg-loadBlue p-2 font-bold text-white"

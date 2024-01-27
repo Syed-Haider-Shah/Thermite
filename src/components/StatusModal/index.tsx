b3fbab5ee9c22f0d56ecc87c8633466831743c43
Ali-Aizaz <50923380+Ali-Aizaz@users.noreply.github.com>
2024-01-27 10:30:48 +0500

    Fixed Errors in statusModal

--- src/components/StatusModal/index.tsx
+++ src/components/StatusModal/index.tsx
@@ -1,4 +1,5 @@
-import { forwardRef } from 'react'
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+import { forwardRef, ForwardRefRenderFunction } from 'react'
 
 forwardRef
 
@@ -7,39 +8,40 @@ type Props = {
   statusClose: () => void
 }
 
-const StatusModal = forwardRef<HTMLDialogElement, Props>(
-  ({ toggleDialog, statusClose }, ref) => {
-    return (
-      <dialog
-        ref={ref}
-        onClick={(e) => {
-          if (e.currentTarget === e.target) {
-            toggleDialog()
-          }
-        }}
-      >
-        <div className="flex flex-col items-center justify-center gap-5 rounded border-2 p-8">
-          <div>Would you like to close the ticket?</div>
-          <div className="flex gap-4">
-            <button
-              className="rounded-5 bg-darkIndigo p-4 px-8 text-white shadow-md transition hover:bg-gradient-to-tr hover:from-darkIndigo hover:to-loadBlue"
-              onClick={() => {
-                statusClose()
-                toggleDialog()
-              }}
-            >
-              YES
-            </button>
-            <button
-              className="rounded-5 bg-darkIndigo p-4 px-8 text-white shadow-md transition-all hover:bg-gradient-to-tr hover:from-darkIndigo hover:to-loadBlue"
-              onClick={toggleDialog}
-            >
-              NO
-            </button>
-          </div>
+const StatusModal: ForwardRefRenderFunction<HTMLDialogElement, Props> = (
+  { toggleDialog, statusClose },
+  ref
+) => {
+  return (
+    <dialog
+      ref={ref}
+      onClick={(e) => {
+        if (e.currentTarget === e.target) {
+          toggleDialog()
+        }
+      }}
+    >
+      <div className="flex flex-col items-center justify-center gap-5 rounded border-2 p-8">
+        <div>Would you like to close the ticket?</div>
+        <div className="flex gap-4">
+          <button
+            className="rounded-5 bg-darkIndigo p-4 px-8 text-white shadow-md transition hover:bg-gradient-to-tr hover:from-darkIndigo hover:to-loadBlue"
+            onClick={() => {
+              statusClose()
+              toggleDialog()
+            }}
+          >
+            YES
+          </button>
+          <button
+            className="rounded-5 bg-darkIndigo p-4 px-8 text-white shadow-md transition-all hover:bg-gradient-to-tr hover:from-darkIndigo hover:to-loadBlue"
+            onClick={toggleDialog}
+          >
+            NO
+          </button>
         </div>
-      </dialog>
-    )
-  }
-)
-export default StatusModal
+      </div>
+    </dialog>
+  )
+}
+export default forwardRef(StatusModal)

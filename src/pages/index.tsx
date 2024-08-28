180efe6e88cf122a38f284f2066b096b122a8e5e
Syed-Haider-Shah <haidershah123123123@gmail.com>
2024-08-28 17:58:30 +1000

    Access added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 
 import { useCallback, useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
@@ -154,12 +155,13 @@ const Home = () => {
               Login
             </Button>
             <div className="ml-2.5 flex flex-col gap-2 text-sm md:flex-row">
-              <div className="text-gray md:text-base">
-                Need help with something?
-              </div>
-              <button className="text-left font-medium text-loadBlue md:text-base">
-                Contact Support
-              </button>
+              <div className="text-gray md:text-base"> How do I log in?</div>
+              <Link
+                href="https://github.com/Syed-Haider-Shah/Thermite?tab=readme-ov-file#getting-started"
+                className="text-left font-medium text-loadBlue md:text-base"
+              >
+                Click Here
+              </Link>
             </div>
           </form>
         </div>
